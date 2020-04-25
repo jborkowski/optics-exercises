aafa957b945448029054c970a4396be7cd2b5193
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-04-25 15:25:25 +0200

    Add Custom Indexted structure impl

--- src/IndexableStructures.hs
+++ src/IndexableStructures.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE RankNTypes #-}
 module IndexableStructures where
 
 import Control.Lens
 import qualified Data.Map as M
 import qualified Data.Set as S
+import qualified Data.Text as T
 
 -- Fill in the blanks
 ex11 = ["Larry", "Curly", "Moe"] & ix 1 .~ "Wiggly"
@@ -19,4 +24,23 @@ ex14 = S.fromList ['a', 'e', 'i', 'o', 'u']
   & at 'y' ?~ ()
   & at 'i' .~ Nothing
 -- Res ex14: S.fromList "aeouy"
+
+
+-- Custom Indexed Structures -
+newtype InsensitiveCase v = InsensitiveCase
+  { _unInsensitiveCase :: M.Map T.Text v
+  } deriving Show
+
+makeLenses ''InsensitiveCase
+
+type instance Index (InsensitiveCase v) = T.Text
+type instance IxValue (InsensitiveCase v) = v
+
+instance Ixed (InsensitiveCase v) where
+  ix :: T.Text -> Traversal' (InsensitiveCase v) v
+  ix k = unInsensitiveCase . ix (T.toLower k)
+
+instance At (InsensitiveCase v) where
+  at :: T.Text -> Lens' (InsensitiveCase v) (Maybe v)
+  at k = unInsensitiveCase . at (T.toLower k)
 
